de0be931074a9c1abfa1f66ca023e64041f327ef
Chibuzo Ekwue <chieezy5@gmail.com>
2023-09-26 18:34:58 +0100

    minor error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Details from "./components/Details/Details";
 import Feature from "./components/Feature/Feature";
-import Footer from "./components/Footer/Footer";
 import GetApp from "./components/GetApp/GetApp";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
@@ -17,7 +16,6 @@ function App() {
 			<Feature />
 			<Testimony />
 			<GetApp />
-			{/* <Footer /> */}
 		</>
 	);
 }
